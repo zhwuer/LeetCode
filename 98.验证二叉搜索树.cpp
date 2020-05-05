14954e6c6d125da9e1123c0b925309bcac3a9d16
Jartus <wjt12137@163.com>
2020-05-05 17:24:43 +0800

    98

---
+++ 98.验证二叉搜索树.cpp
@@ -0,0 +1,77 @@
+/*
+ * @lc app=leetcode.cn id=98 lang=cpp
+ *
+ * [98] 验证二叉搜索树
+ *
+ * https://leetcode-cn.com/problems/validate-binary-search-tree/description/
+ *
+ * algorithms
+ * Medium (29.56%)
+ * Likes:    544
+ * Dislikes: 0
+ * Total Accepted:    104.4K
+ * Total Submissions: 342.6K
+ * Testcase Example:  '[2,1,3]'
+ *
+ * 给定一个二叉树，判断其是否是一个有效的二叉搜索树。
+ * 
+ * 假设一个二叉搜索树具有如下特征：
+ * 
+ * 
+ * 节点的左子树只包含小于当前节点的数。
+ * 节点的右子树只包含大于当前节点的数。
+ * 所有左子树和右子树自身必须也是二叉搜索树。
+ * 
+ * 
+ * 示例 1:
+ * 
+ * 输入:
+ * ⁠   2
+ * ⁠  / \
+ * ⁠ 1   3
+ * 输出: true
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入:
+ * ⁠   5
+ * ⁠  / \
+ * ⁠ 1   4
+ * / \
+ * 3   6
+ * 输出: false
+ * 解释: 输入为: [5,1,4,null,null,3,6]。
+ * 根节点的值为 5 ，但是其右子节点值为 4 。
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ */
+// #include <iostream>
+// struct TreeNode {
+//     int val;
+//     TreeNode *left;
+//     TreeNode *right;
+//     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+// };
+
+class Solution {
+public:
+    bool doit(TreeNode* root, long low, long high) {
+        if (!root) return true;
+        if (root->val > low && root->val < high) {
+            return doit(root->left, low, root->val) && doit(root->right, root->val, high);
+        } else {
+            return false;
+        }
+    }
+    bool isValidBST(TreeNode* root) {
+        return doit(root, LONG_MIN, LONG_MAX);
+    }
+};
+// @lc code=end
+
